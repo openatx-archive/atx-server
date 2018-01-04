61751cfd13c2b417f9e902710e4f74ec64d325cf
shengxiang <codeskyblue@gmail.com>
2018-01-04 09:56:00 +0800

    ignore info update log

--- httplog.go
+++ httplog.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"runtime"
 
 	accesslog "github.com/mash/go-accesslog"
@@ -25,6 +26,10 @@ type HTTPLogger struct {
 // Example
 // [I 170227 14:47:16 web:1946] 200 GET /api/v1/devices (10.240.185.65) 28.00ms
 func (l HTTPLogger) Log(record accesslog.LogRecord) {
+	// update info too many just ignore
+	if record.Method == "POST" && regexp.MustCompile(`/devices/[^/]+/info`).MatchString(record.Uri) {
+		return
+	}
 	logger.Println(fmt.Sprintf("\b] \033[0;m%d %s %s (%s) %.2fms", record.Status, record.Method, record.Uri, record.Ip,
 		float64(record.ElapsedTime.Nanoseconds()/1000)/1000.0))
 }
